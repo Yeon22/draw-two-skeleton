3d0980ee2ef49cc4440104151ce705d29781469c
su <su.bak@litness.live>
2022-12-05 14:30:41 +0900

    Draw two skeletons with aihub pose data and blaze pose data

---
+++ index.js
@@ -0,0 +1,377 @@
+const aihub = {
+  Nose: { x: -2.675819158554077, y: 168.58432006835938, z: 11.600151062011719 },
+  "Left Eye": {
+    x: 0.4500793516635895,
+    y: 172.4015655517578,
+    z: 9.325220108032227,
+  },
+  "Right Eye": {
+    x: -5.7838521003723145,
+    y: 172.01315307617188,
+    z: 9.520073890686035,
+  },
+  "Left Ear": {
+    x: 5.658937931060791,
+    y: 169.58396911621094,
+    z: 0.7597357630729675,
+  },
+  "Right Ear": {
+    x: -10.381103515625,
+    y: 169.01756286621094,
+    z: 0.8968896269798279,
+  },
+  "Left Shoulder": {
+    x: 13.947094917297363,
+    y: 152.17416381835938,
+    z: -2.267608642578125,
+  },
+  "Right Shoulder": {
+    x: -19.63062858581543,
+    y: 151.6411590576172,
+    z: -1.8503094911575317,
+  },
+  "Left Elbow": {
+    x: 32.43320846557617,
+    y: 171.51614379882812,
+    z: 4.072022438049316,
+  },
+  "Right Elbow": {
+    x: -37.76009750366211,
+    y: 169.5636749267578,
+    z: 5.484236717224121,
+  },
+  "Left Wrist": {
+    x: 9.238578796386719,
+    y: 173.13987731933594,
+    z: -3.0701966285705566,
+  },
+  "Right Wrist": {
+    x: -14.6683988571167,
+    y: 171.4466094970703,
+    z: -3.2217674255371094,
+  },
+  "Left Hip": {
+    x: 8.783893585205078,
+    y: 98.0868148803711,
+    z: -2.381526231765747,
+  },
+  "Right Hip": {
+    x: -13.072553634643555,
+    y: 97.79449462890625,
+    z: -2.4124741554260254,
+  },
+  "Left Knee": {
+    x: 12.98066234588623,
+    y: 55.248043060302734,
+    z: -5.0900187492370605,
+  },
+  "Right Knee": {
+    x: -16.78734016418457,
+    y: 55.51673126220703,
+    z: -5.512589454650879,
+  },
+  "Left Ankle": {
+    x: 17.18448829650879,
+    y: 15.522448539733887,
+    z: -8.682119369506836,
+  },
+  "Right Ankle": {
+    x: -19.289051055908203,
+    y: 14.904340744018555,
+    z: -9.183515548706055,
+  },
+  Neck: {
+    x: -2.6532936096191406,
+    y: 159.36668395996094,
+    z: -1.2217084169387817,
+  },
+  "Left Palm": {
+    x: 4.578279972076416,
+    y: 172.21250915527344,
+    z: -5.0766215324401855,
+  },
+  "Right Palm": {
+    x: -9.603133201599121,
+    y: 170.9121856689453,
+    z: -5.008625507354736,
+  },
+  Back: { x: -1.430905818939209, y: 139.2671356201172, z: -4.412360668182373 },
+  Waist: {
+    x: -2.0389962196350098,
+    y: 117.68463134765625,
+    z: -2.1341934204101562,
+  },
+  "Left Foot": {
+    x: 18.683231353759766,
+    y: 10.570877075195312,
+    z: -0.4048371911048889,
+  },
+  "Right Foot": {
+    x: -20.76650619506836,
+    y: 9.427618980407715,
+    z: -1.0485621690750122,
+  },
+};
+
+let blaze = [
+  {
+    name: "nose",
+    x: 939.4661865234375,
+    y: 289.32794189453125,
+    z: 182.88180541992188,
+  },
+  {
+    name: "leftEyeInner",
+    x: 933.7782592773438,
+    y: 278.35614013671875,
+    z: 154.87014770507812,
+  },
+  {
+    name: "leftEye",
+    x: 930.9749755859375,
+    y: 277.9578552246094,
+    z: 154.96780395507812,
+  },
+  {
+    name: "leftEyeOuter",
+    x: 926.0740356445312,
+    y: 277.616455078125,
+    z: 155.10948181152344,
+  },
+  {
+    name: "rightEyeInner",
+    x: 942.61376953125,
+    y: 278.9076232910156,
+    z: 131.66641235351562,
+  },
+  {
+    name: "rightEye",
+    x: 948.5582275390625,
+    y: 279.0690612792969,
+    z: 131.7032012939453,
+  },
+  {
+    name: "rightEyeOuter",
+    x: 952.3099365234375,
+    y: 279.1078796386719,
+    z: 131.84791564941406,
+  },
+  {
+    name: "leftEar",
+    x: 917.6495361328125,
+    y: 284.636474609375,
+    z: 74.88604736328125,
+  },
+  {
+    name: "rightEar",
+    x: 956.4398803710938,
+    y: 284.0647277832031,
+    z: -27.834075927734375,
+  },
+  {
+    name: "leftMouth",
+    x: 938.7423706054688,
+    y: 299.6182556152344,
+    z: 163.39474487304688,
+  },
+  {
+    name: "rightMouth",
+    x: 948.1992797851562,
+    y: 300.0370178222656,
+    z: 133.917724609375,
+  },
+  {
+    name: "leftShoulder",
+    x: 893.849609375,
+    y: 344.0563659667969,
+    z: 74.0580825805664,
+  },
+  {
+    name: "rightShoulder",
+    x: 980.107177734375,
+    y: 337.02325439453125,
+    z: -178.23989868164062,
+  },
+  {
+    name: "leftElbow",
+    x: 873.5880126953125,
+    y: 287.3226623535156,
+    z: -44.58298110961914,
+  },
+  {
+    name: "rightElbow",
+    x: 1050.423583984375,
+    y: 262.8071594238281,
+    z: -347.7749328613281,
+  },
+  {
+    name: "leftWrist",
+    x: 905.5399169921875,
+    y: 273.6044921875,
+    z: -200.9484100341797,
+  },
+  {
+    name: "rightWrist",
+    x: 970.5254516601562,
+    y: 264.3909606933594,
+    z: -435.13446044921875,
+  },
+  {
+    name: "leftHip",
+    x: 914.0146484375,
+    y: 538.27001953125,
+    z: 117.58561706542969,
+  },
+  {
+    name: "rightHip",
+    x: 956.450927734375,
+    y: 542.9759521484375,
+    z: -118.00703430175781,
+  },
+  {
+    name: "leftKnee",
+    x: 895.9489135742188,
+    y: 691.3758544921875,
+    z: 128.18682861328125,
+  },
+  {
+    name: "rightKnee",
+    x: 968.6422119140625,
+    y: 695.6768798828125,
+    z: -69.85933685302734,
+  },
+  {
+    name: "leftAnkle",
+    x: 876.0036010742188,
+    y: 833.1930541992188,
+    z: 124.41411590576172,
+  },
+  {
+    name: "rightAnkle",
+    x: 973.095703125,
+    y: 853.5397338867188,
+    z: -119.08914947509766,
+  },
+  {
+    name: "leftPinky",
+    x: 923.182373046875,
+    y: 260.0042724609375,
+    z: -228.6923065185547,
+  },
+  {
+    name: "rightPinky",
+    x: 950.85302734375,
+    y: 258.7049865722656,
+    z: -470.6465759277344,
+  },
+  {
+    name: "leftIndex",
+    x: 918.2271118164062,
+    y: 271.3702392578125,
+    z: -229.4653778076172,
+  },
+  {
+    name: "rightIndex",
+    x: 946.6293334960938,
+    y: 274.9108581542969,
+    z: -475.56732177734375,
+  },
+  {
+    name: "leftThumb",
+    x: 918.9566040039062,
+    y: 275.5801086425781,
+    z: -207.43568420410156,
+  },
+  {
+    name: "rightThumb",
+    x: 951.1869506835938,
+    y: 279.34814453125,
+    z: -439.07672119140625,
+  },
+  {
+    name: "leftHeel",
+    x: 862.1048583984375,
+    y: 854.9408569335938,
+    z: 120.64198303222656,
+  },
+  {
+    name: "rightHeel",
+    x: 951.9368286132812,
+    y: 877.658203125,
+    z: -126.91818237304688,
+  },
+  {
+    name: "leftFootIndex",
+    x: 909.6514282226562,
+    y: 850.5900268554688,
+    z: 73.14226531982422,
+  },
+  {
+    name: "rightFootIndex",
+    x: 1036.071044921875,
+    y: 873.17333984375,
+    z: -205.28848266601562,
+  },
+];
+const getCenter = (a, b) => ({
+  x: (a.x + b.x) / 2,
+  y: (a.y + b.y) / 2,
+  z: (a.z + b.z) / 2,
+});
+const getDistance = (a, b) =>
+  Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2 + (a.z - b.z) ** 2);
+const getBlaze = (name) => blaze.find((b) => b.name === name);
+const getDiff = (a, b) => ({ x: a.x - b.x, y: a.y - b.y, z: a.z - b.z });
+
+// 1. 스케일 맞추기
+let aihubSize = getDistance(aihub["Left Shoulder"], aihub["Right Hip"]);
+let blazeSize = getDistance(getBlaze("leftShoulder"), getBlaze("rightHip"));
+let scale = aihubSize / blazeSize;
+
+blaze = blaze.map((item) => ({
+  name: item.name,
+  x: item.x * scale,
+  y: item.y * scale,
+  z: item.z * scale,
+}));
+
+const aihubCenter = getCenter(aihub["Left Shoulder"], aihub["Right Hip"]);
+let blazeCenter = getCenter(getBlaze("leftShoulder"), getBlaze("rightHip"));
+const matchCenter = () => {
+  blazeCenter = getCenter(getBlaze("leftShoulder"), getBlaze("rightHip"));
+  const diff = getDiff(aihubCenter, blazeCenter);
+  blaze = blaze.map((item) => ({
+    name: item.name,
+    x: item.x + diff.x,
+    y: item.y + diff.y,
+    z: item.z + diff.z,
+  }));
+  blazeCenter = getCenter(getBlaze("leftShoulder"), getBlaze("rightHip"));
+};
+matchCenter();
+
+const rotate = () => {
+  const aihubVector = getDiff(aihub["Left Shoulder"], aihubCenter);
+  const blazeVector = getDiff(getBlaze("leftShoulder"), blazeCenter);
+  const aihubV = new THREE.Vector3(aihubVector.x, aihubVector.y, aihubVector.z);
+  const blazeV = new THREE.Vector3(blazeVector.x, blazeVector.y, blazeVector.z);
+  const crossV = new THREE.Vector3().crossVectors(aihubV, blazeV).normalize();
+  const angle = (aihubV.angleTo(blazeV) * 180) / Math.PI;
+  console.log("angle", angle);
+
+  const quaternion = new THREE.Quaternion();
+  quaternion.setFromAxisAngle(crossV, angle);
+
+  blaze = blaze.map((item) => {
+    const itemVector = new THREE.Vector3(item.x, item.y, item.z);
+    const applied = itemVector.applyQuaternion(quaternion);
+    return { name: item.name, x: applied.x, y: applied.y, z: applied.z };
+  });
+};
+
+// rotate();
+// matchCenter();
+
+// pck 계산
+// const pck = getDistance(aihub["Left Shoulder"], aihub["Right Hip"]) * 0.2;
+// console.log("pck", pck);
